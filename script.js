9bdf1f2e9ea573d7d261b4c869d07168ee4706e9
VelocityStacks <103700004+VelocityStacks@users.noreply.github.com>
2022-09-27 23:09:27 -0400

    copied files to directory

---
+++ script.js
@@ -0,0 +1,35 @@
+// window.alert();
+// window.confirm();
+// window.prompt();
+
+var wins = 0;
+
+var function(){
+
+}
+
+var RPS =["Rock", "Paper", "Scissors"];
+// computer input
+
+var playerInput = window.prompt("Pick one of the three: Rock, Paper, or Scissors");
+window.alert(playerInput);
+
+if 
+
+
+function RPS
+
+
+function playGame (){
+    // prompt for RPS
+    // generate computer RPS
+    // check who won
+    // increase stats
+    // display stats
+    var playAgain = window.confirm("Play again?")
+    if (playAgain) {
+        playGame();
+    }
+};
+
+playGame();
